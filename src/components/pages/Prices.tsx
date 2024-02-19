36edf7a61af3f520ac44e996333ed163bdfe34de
fitowashere <fito_washere@icloud.com>
2024-02-19 16:03:14 -0400

    deleted PricingTable.tsx added Prices.tsx

---
+++ src/components/pages/Prices.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PricingTable from '../PricingTable';
+
+const Prices = () => {
+    return (
+        <div>
+            <h1>Prices</h1>
+            <PricingTable />
+        </div>
+    );
+};
+
+export default Prices;
