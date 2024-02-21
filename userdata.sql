3899b85ac2ab9f521f4f8da3895fb8ebc435b49d
carlfrank <carl-frank7@hotmail.com>
2024-02-21 15:00:55 -0500

    Add user_data.sql with signups table

---
+++ userdata.sql
@@ -0,0 +1,8 @@
+USE user_data;
+
+CREATE TABLE signups (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE
+);
