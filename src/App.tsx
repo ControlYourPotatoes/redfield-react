71a874b1f2935b23891dffae5fbd57e35dcd7e23
fitowashere <fito_washere@icloud.com>
2024-02-21 22:28:22 -0400

    modified App.tsx deleted App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 // App.tsx
-import React from 'react';
-// Removed Router import since it's now used in the entry point file
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
-import ContactUs from './components/pages/ContactUs';
 import Prices from './components/pages/Prices';
-
+import ContactUs from './components/pages/ContactUs';
 import GuidedStore from './components/guided_store/GuidedStore';
+
+// MainPage component that includes Home, Prices, and About
+const MainPage = () => (
+  <div>
+    <Home />
+    <Prices />
+    <About />
+  </div>
+);
+
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/About' element={<About />} />
+        <Route path='/' element={<MainPage />} />
         <Route path='/Contact_us' element={<ContactUs />} />
-        <Route path='/Prices' element={<Prices />} />
         <Route path='/GuidedStore' element={<GuidedStore />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   );
