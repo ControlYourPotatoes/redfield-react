eba4fc71e455dfd84b4477e771f9548add125726
ControlYourPotatoes <alexanderpuga78@gmail.com>
2024-02-24 16:28:23 -0400

    validate signup from carlos

--- src/components/guided_store/PersonalInfoComponent.tsx
+++ src/components/guided_store/PersonalInfoComponent.tsx
@@ -1,72 +1,162 @@
-import React from 'react';
-import PuertoRicoMap from '../PuertoRicoMap';
+import React, { useState } from 'react';
 import { Typography, TextField, Grid, Button, Box } from '@mui/material';
+import * as yup from 'yup';
+import PuertoRicoMap from '../PuertoRicoMap'; // Ensure this path matches the location of your PuertoRicoMap component
+
+// Validation schema
+const validationSchema = yup.object({
+  firstName: yup.string().required('First Name is required'),
+  lastName: yup.string().required('Last Name is required'),
+  phone: yup.string().required('Phone is required').matches(/^\d+$/, 'Phone must be only digits'),
+  email: yup.string().email('Invalid email format').required('Email is required'),
+  address: yup.string().required('Address is required'),
+});
 
 const PersonalInfoComponent = () => {
-    return (
-        <div>
-            <Typography variant="h3" gutterBottom>
-                Policy Holder Information
-            </Typography>
-            {/* Wrapper for the form fields */}
-            <Box component="form" sx={{ padding: 2 }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            required
-                            id="firstName"
-                            name="firstName"
-                            label="First Name"
-                            fullWidth
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            required
-                            id="lastName"
-                            name="lastName"
-                            label="Last Name"
-                            fullWidth
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            required
-                            id="phone"
-                            name="phone"
-                            label="Phone"
-                            fullWidth
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            required
-                            id="email"
-                            name="email"
-                            label="Email"
-                            fullWidth
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            required
-                            id="address"
-                            name="address"
-                            label="Address"
-                            fullWidth
-                        />
-                    </Grid>
-                </Grid>
-            </Box>
-            {/* Wrapper with padding for the map component */}
-            <Box sx={{ padding: 2 }}>
-                <PuertoRicoMap />
-            </Box>
-            <Button variant="contained" color="primary" sx={{ mt: 2 }}>
-                Save Information
+  const [formValues, setFormValues] = useState({
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+    address: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormValues({...formValues, [name]: value});
+        // Optionally reset errors
+        setErrors({...errors, [name]: ''});
+    };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // Validate form values
+      await validationSchema.validate(formValues, { abortEarly: false });
+      setErrors({}); // Reset errors if validation succeeds
+
+      // Here, insert the API call logic to send data to your backend
+      const response = await fetch('http://localhost:3001/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          firstName: formValues.firstName,
+          lastName: formValues.lastName,
+          phone: formValues.phone,
+          email: formValues.email,
+          address: formValues.address,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Server responded with an error');
+      }
+
+      console.log('Form submission successful');
+      // Handle success here, e.g., showing a success message or redirecting the user
+    } catch (err) {
+      console.error('Submission error:', err);
+      if (err instanceof yup.ValidationError) {
+        // Transform Yup validation errors to a more manageable structure
+        const formErrors = err.inner.reduce((acc, current) => {
+          acc[current.path] = current.message;
+          return acc;
+        }, {});
+        setErrors(formErrors);
+      }
+      // Handle other errors, e.g., network error or server error
+    }
+  };
+
+  return (
+    <div>
+      <Typography variant="h3" gutterBottom>
+        Policy Holder Information
+      </Typography>
+      <Box component="form" sx={{ padding: 2 }} onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              fullWidth
+              value={formValues.firstName}
+              onChange={handleChange}
+              error={Boolean(errors.firstName)}
+              helperText={errors.firstName || ''}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              fullWidth
+              value={formValues.lastName}
+              onChange={handleChange}
+              error={Boolean(errors.lastName)}
+              helperText={errors.lastName || ''}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="phone"
+              name="phone"
+              label="Phone"
+              fullWidth
+              value={formValues.phone}
+              onChange={handleChange}
+              error={Boolean(errors.phone)}
+              helperText={errors.phone || ''}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="Email"
+              fullWidth
+              value={formValues.email}
+              onChange={handleChange}
+              error={Boolean(errors.email)}
+              helperText={errors.email || ''}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="address"
+              name="address"
+              label="Address"
+              fullWidth
+              value={formValues.address}
+              onChange={handleChange}
+              error={Boolean(errors.address)}
+              helperText={errors.address || ''}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained" color="primary" sx={{ mt: 2 }}>
+              Save Information
             </Button>
-        </div>
-    );
+          </Grid>
+        </Grid>
+      </Box>
+      {/* Displaying the Puerto Rico map component below the form */}
+      <Box sx={{ padding: 2 }}>
+        <PuertoRicoMap />
+      </Box>
+    </div>
+  );
 };
 
 export default PersonalInfoComponent;
