3742aad27a705207e11b108c853e16bb8a4b4f2b
ControlYourPotatoes <alexanderpuga78@gmail.com>
2024-02-26 20:10:20 -0400

    added cors to be able to test in browser

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const usersRoutes = require('./routes/users');
 
 const app = express();
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
