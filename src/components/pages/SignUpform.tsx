ac3bd9970b096689158014429d59512713d504b1
fitowashere <fito_washere@icloud.com>
2024-03-05 17:10:32 -0400

    modified SignUpform.tsx in pages

--- src/components/pages/SignUpform.tsx
+++ src/components/pages/SignUpform.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock, faUser, faPhone, faUserAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Container = styled.div`
 display: flex;
@@ -13,32 +15,44 @@ position: relative;
 `;
 
 const FormContainer = styled.div<{ show: boolean }>`
-flex-direction: column;
-transition: all 0.5s ease;
-opacity: ${({ show }) => (show ? 1 : 0)};
-display: ${({ show }) => (show ? 'flex' : 'none')};
-  width: 100%;
-  max-width: 550px;
-  padding: 20px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  border-radius: 5px;
-  background-color: white;
-  min-height: 550px;
-  color: black;
-  margin-bottom: 20px; /* Adjust spacing between form and button */
+ flex-direction: column;
+ transition: all 0.5s ease;
+ opacity: ${({ show }) => (show ? 1 : 0)};
+ display: ${({ show }) => (show ? 'flex' : 'none')};
+ width: 100%;
+ max-width: 550px;
+ padding: 20px;
+ box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+ border-radius: 5px;
+ background-color: white;
+ min-height: 550px; 
+ color: black;
+ margin-bottom: 20px; /* Adjust spacing between form and button */
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin: 8px 0;
 `;
 
 const StyledInput = styled(Field)`
-  padding: 12px 15px;
-  margin: 8px 0;
+  padding: 12px 15px 12px 35px; /* Adjust left padding to make room for the icon */
   width: calc(100% - 30px);
-  box-sizing: border-box;
   border: 1px solid #ccc;
   border-radius: 4px;
   &:focus {
     border-color: #4A90E2;
     outline: none;
   }
+  box-sizing: border-box;
+`;
+
+const Icon = styled(FontAwesomeIcon)`
+  position: absolute;
+  left: 10px;
+  color: #ccc;
 `;
 
 const FormTitle = styled.h2`
@@ -135,17 +149,62 @@ const ForgotPasswordFormContainer = styled.div`
   color: black;
 `;
 
+const ToggleWrapper = styled.div`
+  display: flex;
+  justify-content: center; // Aligns the toggle container to the right
+  width: 100%; // Ensures the wrapper spans the full width of its parent
+  padding-right: 20px; // Adjust this value to move the toggle button more to the right
+  margin-bottom: 20px; // Adds some space below the toggle button
+`;
+
+const ToggleContainer = styled.div`
+  display: flex;
+  background-color: black;
+  border-radius: 20px;
+  width: 150px;
+  height: 40px;
+  cursor: pointer;
+  position: relative;
+  margin-bottom: 40px; // Add space below the toggle button
+`;
+
+const ToggleIndicator = styled.div<{ isSignUp: boolean }>`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  background-color: lightgray;
+  border-radius: 18px;
+  transition: all 0.2s ease;
+  width: 50%; // Indicator covers half of the container
+  left: ${({ isSignUp }) => (isSignUp ? '50%' : '0')}; // Move indicator based on isSignUp
+  font-size: 14px;
+  font-weight: bold;
+  color: #333;
+  &:before {
+    content: ${({ isSignUp }) => (isSignUp ? '"Sign Up"' : '"Log In"')};
+  }
+`;
+
 const signInSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Required'),
   password: Yup.string().min(6, 'Password is too short').required('Required'),
 });
 
 const signUpSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('Required'),
-  password: Yup.string().min(6, 'Password is too short').required('Required'),
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  password: Yup.string().min(6, 'Password is too short').required('Password is required'),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref('password'), undefined], "Passwords don't match")
-    .required('Required'),
+    .required('Confirming password is required'),
+  firstName: Yup.string().required('First name is required'),
+  lastName: Yup.string().required('Last name is required'),
+  phoneNumber: Yup.string()
+    .matches(/^[0-9]+$/, "Must be only digits")
+    .min(10, 'Must be exactly 10 digits')
+    .max(10, 'Must be exactly 10 digits')
+    .required('Phone number is required'),
 });
 
 // Yup validation schema for the forgot password email
@@ -195,10 +254,13 @@ const SignInSignUpPage = () => {
             <Form>
               <FormTitle>Forgot Password</FormTitle>
               <StyledLabel>Email</StyledLabel>
+              <InputWrapper>
+              <Icon icon={faEnvelope} />
               <StyledInput name="email" type="email" placeholder="Enter your email" />
               {errors.email && touched.email ? (
                 <div style={{ color: 'red', marginTop: '0.5rem' }}></div>
               ) : null}
+              </InputWrapper>
               <ErrorMessageContainer>
                 <ErrorMessage name="email" component="div" />
               </ErrorMessageContainer>
@@ -214,25 +276,42 @@ const SignInSignUpPage = () => {
     );
   }
 
+   // Calculate the dynamic style based on isSignUp state
+   const containerStyle = {
+    justifyContent: isSignUp ? 'flex-start' : 'center',
+    paddingTop: isSignUp ? '150px' : '0', // Or any other value that suits your design
+  };
+
+
   return (
-    <Container>
+    <Container style={containerStyle}>
       <FormContainer show={!isSignUp}>
         <Formik
           initialValues={{ email: '', password: '' }}
           validationSchema={signInSchema}
-          onSubmit={(values) => console.log('Sign In', values)}
+          onSubmit={(values) => console.log('Log In', values)}
         >
           {({ isSubmitting }) => (
             <Form>
-              <FormTitle>Sign In</FormTitle>
+              <ToggleWrapper>
+               <ToggleContainer  onClick={toggleForm}>
+                <ToggleIndicator isSignUp={isSignUp} />
+               </ToggleContainer >
+              </ToggleWrapper>
               <StyledLabel>Email</StyledLabel>
+              <InputWrapper>
+               <Icon icon={faEnvelope} />
               <StyledInput name="email" type="email" placeholder="Enter email" />
+              </InputWrapper>
               <ErrorMessageContainer>
                 <ErrorMessage name="email" component="div" />
               </ErrorMessageContainer>
 
               <StyledLabel>Password</StyledLabel>
+              <InputWrapper>
+               <Icon icon={faLock} />
               <StyledInput name="password" type="password" placeholder="Enter password" />
+              </InputWrapper>
               <ErrorMessageContainer>
                 <ErrorMessage name="password" component="div" />
               </ErrorMessageContainer>
@@ -240,6 +319,7 @@ const SignInSignUpPage = () => {
                 <Checkbox type="checkbox" name="rememberMe" />
                 Remember Me
               </CheckboxLabel>
+              
               <SubmitButton type="submit" disabled={isSubmitting}>
              {isSignUp ? 'Sign Up' : 'Sign In'}
              </SubmitButton>
@@ -255,27 +335,67 @@ const SignInSignUpPage = () => {
 
       <FormContainer show={isSignUp}>
         <Formik
-          initialValues={{ email: '', password: '', confirmPassword: '' }}
+          initialValues={{  firstName: '', lastName:'', phoneNumber: '', email: '', password: '', confirmPassword: ''}}
           validationSchema={signUpSchema}
           onSubmit={(values) => console.log('Sign Up', values)}
         >
           {({ isSubmitting }) => (
             <Form>
-              <FormTitle>Sign Up</FormTitle>
+              <ToggleWrapper>
+               <ToggleContainer  onClick={toggleForm}>
+                <ToggleIndicator isSignUp={isSignUp} />
+               </ToggleContainer >
+              </ToggleWrapper>
               <StyledLabel>Email</StyledLabel>
+              <InputWrapper>
+              <Icon icon={faEnvelope} />
               <StyledInput name="email" type="email" placeholder="Enter email" />
+              </InputWrapper>
               <ErrorMessageContainer>
                 <ErrorMessage name="email" component="div" />
               </ErrorMessageContainer>
 
+              <StyledLabel>First Name</StyledLabel>
+                <InputWrapper>
+                  <Icon icon={faUser} />
+                  <StyledInput name="firstName" type="text" placeholder="First Name" />
+                </InputWrapper>
+                <ErrorMessageContainer>
+                 <ErrorMessage name="firstName" component="div" />
+                </ErrorMessageContainer>
+
+              <StyledLabel>last Name</StyledLabel>
+                <InputWrapper>
+                <Icon icon={faUserAlt} />
+                  <StyledInput name="lastName" type="text" placeholder="Last Name" />
+                </InputWrapper>
+                <ErrorMessageContainer>
+                 <ErrorMessage name="lastName" component="div" />
+                </ErrorMessageContainer>
+
+              <StyledLabel>Phone Number</StyledLabel>
+               <InputWrapper>
+                <Icon icon={faPhone} />
+                <StyledInput name="phoneNumber" type="tel" placeholder="Phone Number" />
+               </InputWrapper>
+                <ErrorMessageContainer>
+                 <ErrorMessage name="phoneNumber" component="div" />
+                </ErrorMessageContainer>
+
               <StyledLabel>Password</StyledLabel>
+              <InputWrapper>
+              <Icon icon={faLock} />
               <StyledInput name="password" type="password" placeholder="Enter password" />
+              </InputWrapper>
               <ErrorMessageContainer>
                 <ErrorMessage name="password" component="div" />
               </ErrorMessageContainer>
 
               <StyledLabel>Confirm Password</StyledLabel>
+              <InputWrapper>
+              <Icon icon={faLock} />
               <StyledInput name="confirmPassword" type="password" placeholder="Confirm Password" />
+              </InputWrapper>
               <ErrorMessageContainer>
                 <ErrorMessage name="confirmPassword" component="div" />
               </ErrorMessageContainer>
@@ -287,9 +407,7 @@ const SignInSignUpPage = () => {
         </Formik>
       </FormContainer>
 
-      <SwitchButton onClick={toggleForm}>
-        {isSignUp ? "Go to Sign In" : "Go to Sign Up"}
-      </SwitchButton>
+     
     </Container>
   );
 };
