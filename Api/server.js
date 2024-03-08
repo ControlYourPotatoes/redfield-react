d74dc858f0d40c99198f16474389a83e8cb36e8e
ControlYourPotatoes <alexanderpuga78@gmail.com>
2024-03-08 13:29:48 -0400

    Update recipient email address in sendNotificationEmail function

--- Api/server.js
+++ Api/server.js
@@ -46,7 +46,7 @@ let transporter = nodemailer.createTransport({
 function sendNotificationEmail(message) {
   let mailOptions = {
     from: process.env.GMAIL_USER, // place your email
-    to: 'alexanderpuga78@gmail.com', // Set the recipient email address
+    to: 'mercedes.diaz@holbertonschool.com', // Set the recipient email address
     subject: 'Hurricane Alert',
     text: message,
   };
