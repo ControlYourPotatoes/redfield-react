28ed8c804f74a960c441daa91c73d53df8646731
carlfrank <carl-frank7@hotmail.com>
2024-03-11 15:39:22 -0400

    Refactor InvestorDashboard component

--- src/components/Dashboard/InvestorDashboard.tsx
+++ src/components/Dashboard/InvestorDashboard.tsx
@@ -1,54 +1,61 @@
 import React from 'react';
-import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button, Grid, Typography } from '@mui/material';
+import { Box, Container, Paper, Typography, TextField, Button, InputAdornment, Grid } from '@mui/material';
+import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
 
 const InvestorDashboard = () => {
   return (
-    <Container>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography variant="h4" gutterBottom>
-            Azurance
+    // Wrapping Box to make the container center aligned
+    <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+      <Container maxWidth="sm">
+        <Box my={4} display="flex" flexDirection="column" alignItems="center">
+          <Typography variant="h4" component="h1" gutterBottom>
+            Invest in Redfield Insurance
           </Typography>
-          <Typography variant="subtitle1" gutterBottom>
-            Decentralized insurance platform that providing flexible conditions on-chain
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          {/* This is where your table will go */}
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>INSURANCE NAME</TableCell>
-                  <TableCell align="right">ASSET</TableCell>
-                  <TableCell align="right">BENEFIT</TableCell>
-                  <TableCell align="right">UTILIZATION</TableCell>
-                  <TableCell align="right">TOTAL SUPPLY</TableCell>
-                  <TableCell align="right">EXPIRATION</TableCell>
-                  <TableCell align="right">ACTION</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {/* Example row */}
-                <TableRow
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    10minTestCovid
-                  </TableCell>
-                  <TableCell align="right">USD Tether</TableCell>
-                  <TableCell align="right">10x</TableCell>
-                  <TableCell align="right">0%</TableCell>
-                  <TableCell align="right">0.00 USDT</TableCell>
-                  <TableCell align="right">TIME OVER</TableCell>
-                  <TableCell align="right"><Button variant="contained">Buy</Button></TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-      </Grid>
-    </Container>
+          <Paper elevation={3} sx={{ p: 2, width: '100%', mt: 2 }}>
+            <Typography variant="h6" gutterBottom>
+              Invest in Redfield Insurance
+            </Typography>
+            <Grid container spacing={3} alignItems="center">
+              <Grid item xs={12}>
+                <TextField
+                  label="From: RDF Policy Insurance"
+                  fullWidth
+                  variant="outlined"
+                  InputProps={{
+                    endAdornment: <InputAdornment position="end">RDF</InputAdornment>,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} display="flex" justifyContent="center">
+                <SwapHorizIcon fontSize="large" />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="To: USDC"
+                  fullWidth
+                  variant="outlined"
+                  InputProps={{
+                    endAdornment: <InputAdornment position="end">USDC</InputAdornment>,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body2" gutterBottom>Price: 1 RDF per 500 USDC</Typography>
+                <Typography variant="body2" gutterBottom>Slippage Tolerance: 0.1%</Typography>
+                <Typography variant="body2" gutterBottom>Minimum received: 0 USDC</Typography>
+                <Typography variant="body2" gutterBottom>Price Impact: {"<"} 0.01%</Typography>
+                <Typography variant="body2" gutterBottom>Route: RDF {" > "} USDC</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Button variant="contained" color="primary" fullWidth>
+                  Invest in Redfield Insurance
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
